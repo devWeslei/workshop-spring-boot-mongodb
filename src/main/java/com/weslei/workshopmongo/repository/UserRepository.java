0f565800f0fae8c945334ef7bae4bb00f537eb23
Weslei Tiago <weslei.tiago123@gmail.com>
2024-02-23 22:16:14 -0300

    finished project

---
+++ src/main/java/com/weslei/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.weslei.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.weslei.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
